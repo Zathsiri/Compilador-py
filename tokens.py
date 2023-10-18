34d8c33bf038221324444ccfb659c6318c341901
Gilberto Berttolini <85074402+Zathsiri@users.noreply.github.com>
2023-10-17 22:51:53 -0600

    corrigiendo unos errores
    tengo unos cuentos errores donde estoy escribiendo mal unas tokens como instrucciones dentro del parser

--- tokens.py
+++ tokens.py
@@ -1,5 +1,5 @@
 # Definición de tokens
-tokens = (
+tokens = [
     'NEWLINE',
     'SEMICOLON',
     'PAR_LEFT',
@@ -35,10 +35,12 @@ tokens = (
     'RIGHT_BRACE',
     'IF',
     'ELSE',
-    'WRITE'
+    'WRITE',
+    'PRINCIPAL',
+    'FOR',
+    'WHILE'
 
-
-)
+]
 
 # Definición de reglas para tokens
 t_NEWLINE = r'\\n'
@@ -63,20 +65,20 @@ t_LESS_EQUAL = r'<='
 t_GREATER_EQUAL = r'>='
 t_DOUBLE_EQUAL = r'=='
 t_BRACKET_LEFT = r'\['  # Corchete izquierdo
-t_RBRACKET_RIGHT = r'\]'  # Corchete derecho
+t_BRACKET_RIGHT = r'\]'  # Corchete derecho
 t_ID = r'[a-zA-Z][a-zA-Z0-9]*'
 t_DIGIT_INTEGER = r'[0-9]+'
 t_DIGIT_DECIMAL = r'[0-9]+\.[0-9]+'
 t_LEFT_BRACE = r'\{'
 t_RIGHT_BRACE = r'\}'
-
 # Tokens para tipos de datos
 t_INT = r'int'
 t_FLOAT = r'float'
-t_VOID = r'void'
 t_CHAR = r'char'
 #Token para la funcion 
 t_FUNCTION = r'function'
+#Token unico para la funcion de tipo void 
+t_VOID = r'void'
 #token del programa 
 t_PROGRAM = r'program'
 #token de la declariacion de las variables
@@ -86,3 +88,8 @@ t_IF = r'if'
 t_ELSE = r'else'
 #Token del write 
 t_WRITE = r'write'
+#token del principal
+t_PRINCIPAL = r'Principal'
+#Token de los ciclos
+t_FOR = r'for'
+t_WHILE = r'while'
